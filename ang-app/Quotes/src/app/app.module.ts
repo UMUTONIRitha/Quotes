c1fa299c2306efc37f63c777962c017943a6547a
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-31 14:34:47 +0200

    add quote form

--- ang-app/Quotes/src/app/app.module.ts
+++ ang-app/Quotes/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    DateCountPipe
+    DateCountPipe,
+    GoalFormComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
