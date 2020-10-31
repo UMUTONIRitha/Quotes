c09044468df891caa540359cb281524fa3172af0
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-31 12:12:17 +0200

    add date in class quote

--- ang-app/Quotes/src/app/quote.ts
+++ ang-app/Quotes/src/app/quote.ts
@@ -1,5 +1,7 @@
 export class Quote {
-    id: number;
-  name: string;
-  author: string;
+    showAuthor: boolean;
+    constructor(public id: number,public name: string,public author: string,public completeDate: Date){
+      this.showAuthor=false;
+    }
+  
 }
