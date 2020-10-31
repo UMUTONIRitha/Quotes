93770eb418b9b23b8228d82b55226a6dfeb3efe6
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-31 11:11:27 +0200

    add show auther event

--- ang-app/Quotes/src/app/quote/quote.component.ts
+++ ang-app/Quotes/src/app/quote/quote.component.ts
@@ -8,13 +8,17 @@ import { Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    {id:1, name:'The greatest glory in living lies not in never falling, but in rising every time we fall.',author:'Nelson Mandela'},
-    {id:2,name:'The way to get started is to quit talking and begin doing.',author:'Walt Disney'},
-    {id:3,name:'If life were predictable it would cease to be life, and be without flavor.',author:'Eleanor Roosevelt'},
-    {id:4, name:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",author:'Steve Jobs'},
-    {id:5,name:"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",author:'Oprah Winfrey'},
-    {id:6,name:"Life is what happens when you're busy making other plans.",author:'John Lennon'},
+    new Quote(1,'The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela'),
+   new Quote(2,'The way to get started is to quit talking and begin doing.','Walt Disney'),
+    new Quote(3,'If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt'),
+    new Quote(4,"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",'Steve Jobs'),
+    new Quote(5,"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",'Oprah Winfrey'),
+   new Quote(6,"Life is what happens when you're busy making other plans.",'John Lennon'),
   ];
+  toggleDetails(index){
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+  }
+ 
   constructor() { }
 
   ngOnInit(): void {
