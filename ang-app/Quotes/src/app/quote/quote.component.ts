e46feca7087b26c9f2f1df5684ce5f071e034be2
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-31 12:11:28 +0200

    add date

--- ang-app/Quotes/src/app/quote/quote.component.ts
+++ ang-app/Quotes/src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1,'The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela'),
-   new Quote(2,'The way to get started is to quit talking and begin doing.','Walt Disney'),
-    new Quote(3,'If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt'),
-    new Quote(4,"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",'Steve Jobs'),
-    new Quote(5,"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",'Oprah Winfrey'),
-   new Quote(6,"Life is what happens when you're busy making other plans.",'John Lennon'),
+    new Quote(1,'The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela',new Date(2020,3,14)),
+   new Quote(2,'The way to get started is to quit talking and begin doing.','Walt Disney',new Date(2015,5,24)),
+    new Quote(3,'If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt',new Date(2020,3,14)),
+    new Quote(4,"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",'Steve Jobs',new Date(1998,4,30)),
+    new Quote(5,"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",'Oprah Winfrey',new Date(2002,9,4)),
+   new Quote(6,"Life is what happens when you're busy making other plans.",'John Lennon',new Date(2020,10,14)),
   ];
   toggleDetails(index){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
