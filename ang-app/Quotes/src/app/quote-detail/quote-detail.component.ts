0b67e4568325350b97b1da270cc0914b812125af
UMUTONIRitha <rithamarie9@gmail.com>
2020-10-31 11:36:25 +0200

    add output in export class

--- ang-app/Quotes/src/app/quote-detail/quote-detail.component.ts
+++ ang-app/Quotes/src/app/quote-detail/quote-detail.component.ts
@@ -9,6 +9,11 @@ import { Quote } from '../quote';
 export class QuoteDetailComponent implements OnInit {
 
   @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
