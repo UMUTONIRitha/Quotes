092f181b06193512616212772f45c31763ca53a0
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-01 11:39:58 +0200

    add publisher pusher's space

--- ang-app/Quotes/src/app/quote-form/quote-form.component.ts
+++ ang-app/Quotes/src/app/quote-form/quote-form.component.ts
@@ -8,12 +8,13 @@ import { Quote } from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote = new Quote(0,"","",new Date());
-  @Output() addQuote = new EventEmitter<Quote>();
   
-    submitQuote(){
-  this.addQuote.emit(this.newQuote);
-    }
+  newQuote = new Quote(0,"","","",new Date());
+@Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {
