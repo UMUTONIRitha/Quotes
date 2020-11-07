24c9ffda05a5a33c33271333072f273ae0b5f89c
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-07 11:42:22 +0200

    add a component called users

---
+++ ang-app/Github-Search/src/app/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import{User} from '../user';
+import { UserService } from '../user-service/user.service';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+user:User;
+userName="";
+submitRepos(){
+
+}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
