d8971e02fa9664183fca8c7dfb75098f23c9d9be
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-08 22:57:48 +0200

    save changes

--- ang-app/Github-Search/src/app/app-routing.module.ts
+++ ang-app/Github-Search/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AboutGithubComponent } from './about-github/about-github.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: 'users', component: UsersComponent},
+  { path: 'about-github', component:AboutGithubComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
